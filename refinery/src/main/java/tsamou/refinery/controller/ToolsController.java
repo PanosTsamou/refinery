9103685919e62591ceb1112ded9f37fc0ab1a64a
Panagiotis <tsamou7@gmail.com>
2024-08-22 00:49:51 +0100

    finish controllers and add actuators table

--- refinery/src/main/java/tsamou/refinery/controller/ToolsController.java
+++ refinery/src/main/java/tsamou/refinery/controller/ToolsController.java
@@ -1,4 +1,42 @@
 package tsamou.refinery.controller;
 
+import jdk.jfr.BooleanFlag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import tsamou.refinery.models.Tools;
+import tsamou.refinery.repository.ToolsRepository;
+import tsamou.refinery.utilities.ToolsHelper;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
 public class ToolsController {
+
+    @Autowired
+    ToolsRepository toolsRepository;
+
+    @GetMapping ("/tools")
+    public List<Tools> getAllTools(){
+        return toolsRepository.findAll();
+    }   @GetMapping ("/tools/{id}")
+    public Optional<Tools> getToolsById(@PathVariable Long id){
+        return toolsRepository.findById(id);
+    }
+
+    @PostMapping ("/tools")
+    public ResponseEntity<Tools> saveTools (@RequestBody Tools tool){
+        toolsRepository.save(tool);
+        return new ResponseEntity<>(tool, HttpStatus.CREATED);
+    }
+
+    @PutMapping ("/tools/{id}")
+    public ResponseEntity<Tools> updateTools (@RequestBody Tools tool, @PathVariable Long id){
+        Tools toolToBeUpdated = toolsRepository.findById(id).get();
+        ToolsHelper.updateTools(toolToBeUpdated, tool);
+        toolsRepository.save(toolToBeUpdated);
+        return new ResponseEntity<>(toolToBeUpdated, HttpStatus.OK);
+    }
 }
--- refinery/src/main/java/tsamou/refinery/models/Unit.java
+++ refinery/src/main/java/tsamou/refinery/models/Unit.java
@@ -43,4 +43,12 @@ public class Unit implements Serializable {
     public void setNumber(Integer number) {
         this.number = number;
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
